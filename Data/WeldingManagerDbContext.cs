152e95e40b5d54dc38e4ccec811b23fddb7743f6
Saman-Asghari <samansadat1383@gmail.com>
2024-10-09 18:04:22 +0330

    Adding the classes and database

---
+++ Data/WeldingManagerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeldingManagerApp.Models;
+
+namespace WeldingManagerApp.Data
+{
+    public class WeldingManagerDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = localhost; Initial Catalog = WeldingManagerDB; Integrated Security = True; Connect Timeout = 30; Encrypt = True; Trust Server Certificate = True; Application Intent = ReadWrite; Multi Subnet Failover = False");
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeldingManagerApp.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Description { get; set; } = null;
+        public string PieceName { get; set; }
+        public int Level { get; set; }
+        public int BasePrice { get; set; }
+        public int lastPrice { get; set; }
+        public DateTime EstimatedTime { get; set; }
+
+        //foreign key to the customer
+        public int CustomerId { get; set; }
+        // Navigation property for the customer
+        public Customer Customer { get; set; }
+
+    }
+}
